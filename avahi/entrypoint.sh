9f15850d4f89701f93cb37b9cfdfa155b0e949ad
tskon-kz <tsekhmeister.k+kz@gmail.com>
2026-08-22 15:01:29 +0300

    fix interfaces

--- avahi/entrypoint.sh
+++ avahi/entrypoint.sh
@@ -11,4 +11,10 @@ envsubst '${AIRPRINT_NAME} ${PRINTER_NAME} ${PRINTER_PRODUCT}' \
   > /etc/avahi/services/airprint.service
 cp /opt/hs-print/avahi-daemon.conf.template /etc/avahi/avahi-daemon.conf
 
+# Restrict advertising to the physical LAN interface(s) so iOS does not receive
+# unreachable docker bridge A-records for home-server.local.
+if [[ -n "${AVAHI_INTERFACES:-}" ]]; then
+  sed -i "/^\[server\]/a allow-interfaces=${AVAHI_INTERFACES}" /etc/avahi/avahi-daemon.conf
+fi
+
 exec avahi-daemon --no-chroot --debug
